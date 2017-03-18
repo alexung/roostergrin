8ed1c873dc09c229af28bd1f13991df175684125
Alex Ung <alexung.ucsb@gmail.com>
2017-03-17 17:35:17 -0700

    clean up logic around replacing text

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -12,15 +12,15 @@ class PatientsController < ApplicationController
 	def replace_text(split_text, patient)
 		split_text.map do |t| 
 			if t[0] === '@' 
-				non_letters = ['']
+				non_letters = ''
 				until t[-1] =~ /[A-Za-z]/
-					non_letters.push(t[-1])
+					non_letters += t[-1]
 					t = t.chomp(t[-1])
 				end
 					db_value = t[1..-1] === 'phone_number' ?
 						ActionController::Base.helpers.number_to_phone(patient[t[1..-1]], area_code: true) :
 						patient[t[1..-1]]
-					db_value + non_letters.reverse.join('')
+					db_value + non_letters
 			else 
 				t 
 			end
